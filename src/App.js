826bdbf630a89ffdf25a79983574f3b16409b45f
juanserodriguez29 <juanserodriguez29@hotmail.com>
2024-01-19 12:01:57 -0500

    Add all files from local project to reposotory

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import axios from "axios";
+import "./header.css";
+import "./content.css";
+import "./article.css";
 
 function App() {
+  const [photos, setPhotos] = useState([])
+  const open = url => window.open(url)
+  console.log({photos})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: "" }}
+          onSubmit={(values) =>
+            axios
+              .get(
+                `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+                {
+                  headers: {
+                    Authorization:
+                      "Client-ID 8AjWqDpdq4WF9MaHe-VzZEGFwDGfRVHExAHpcpRajUw",
+                  },
+                }
+              )
+              .then((response) => {
+                setPhotos(response.data.results)
+              })
+              .catch((err) => {
+                console.log(err.response);
+              })
+          }
         >
-          Learn React
-        </a>
+          <Form>
+            <label>Busca:</label>
+            <Field name="search" type="text" />
+          </Form>
+        </Formik>
       </header>
+      <div className="container">
+        <div className="center">
+          {photos.map(photo => 
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular}/>
+              <p>{[photo.description, photo.alt_description].join(" - ")}</p>
+            </article>)}
+        </div>
+      </div>
     </div>
   );
 }
